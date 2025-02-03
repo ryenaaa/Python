19c8df8b3b5e8a430674768ea9b48636daf0ec74
ryenaaa <morexilast@gmail.com>
2025-02-03 18:00:33 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+print ("Program OOP ")
+
+class RekeningBank :
+
+  def __init__(self,tabungan):
+    self.tabungan = tabungan
+
+  def CekSaldo(self):
+    print(f"sisa saldo anda {self.tabungan}")
+
+  def Menabung(self):
+    tambah_tabungan = int(input("Masukan Jumlah yang ingin ditabungkan : "))
+    tambah_tabungan += self.tabungan
+
+  def Menarik(self):
+    kurang_tabungan = int(input("Jumlah tunai yang akan ditarik : "))
+    if self.tabungan < kurang_tabungan  :
+      print(f"Saldo tersisa Rp.{self.tabungan}.Penarikan tunai tidak dapat dilakukan.")
+
+    else :
+      self.tabungan -= kurang_tabungan
+      
+      
+  
